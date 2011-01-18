507c408b51e031b6bdf49d2a4cfda742653a7954
P A Alberti <peter.adler.alberti@gmail.com>
2011-01-18 17:28:08 +0100

    dan-frak

---
+++ dan-frak/buildscript.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+LANG=dan-frak
+for i in *.tif
+  do echo $i:
+  tesseract $i ${i%.tif} nobatch box.train
+done
+unicharset_extractor *.box
+mftraining *.tr
+cntraining *.tr
+for i in inttemp normproto pffmtable unicharset
+  do mv -f $i $LANG.$i
+done
+wordlist2dawg number $LANG.number-dawg $LANG.unicharset
+wordlist2dawg punc $LANG.punc-dawg $LANG.unicharset
+wordlist2dawg word_list $LANG.word-dawg $LANG.unicharset
+wordlist2dawg frequency_list $LANG.freq-dawg $LANG.unicharset
+combine_tessdata $LANG.
