7c869fb3d4f4ee0e6103b6eafc4a9439d70274e6
P A Alberti <peter.adler.alberti@gmail.com>
2011-02-12 21:37:23 +0100

    testing autogenerated numbers

--- dan-frak/buildscript.sh
+++ dan-frak/buildscript.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 LANG=dan-frak
 for i in *.tif
   do echo $i:
@@ -9,6 +9,24 @@ mftraining *.tr
 cntraining *.tr
 for i in inttemp normproto pffmtable unicharset
   do mv -f $i $LANG.$i
+done
+rm -f number
+function integerrange {
+  temp=$1
+  while test $temp -le $2
+  do 
+     echo $temp$3
+     temp=$[ $temp + 1 ]
+  done
+}
+integerrange -9999 9999 >> number
+for i in $(integerrange 0 9)
+do
+   integerrange -99 99 ,$i >> number
+done
+for i in $(integerrange 0 99)
+do
+   integerrange 0 99 $(printf ,%02d $i) >> number
 done
 wordlist2dawg number $LANG.number-dawg $LANG.unicharset
 wordlist2dawg punc $LANG.punc-dawg $LANG.unicharset
